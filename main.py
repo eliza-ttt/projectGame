0eb47b6a839d0d545c2bf5715eb09890f945dd45
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2020-12-18 02:04:33 +0600

    foundation

--- main.py
+++ main.py
@@ -1,31 +1,62 @@
 import pygame
-import random
 
-WIDTH = 
-HEIGHT =
-FPS = 
-
+screen_width=1280 
+screen_height=960
 
 pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Game")
 
-running = True
-while running:
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200, 200, 200)
+
+ball_speed_x = 7
+ball_speed_y = 7
+player_speed = 0
+
+while True:
     
     clock.tick(FPS)
    
     for event in pygame.event.get():
        
         if event.type == pygame.QUIT:
-            running = False
-
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.type == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
    
-    screen.fill(BLACK)
+    ball_animation()
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
    
     pygame.display.flip()
-
-pygame.quit()
+    clock.tick(60)
