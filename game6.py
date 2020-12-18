8afd825678b2ccecd4ca1776e9f160c80fcde096
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2020-12-18 18:43:17 +0600

    Update game6.py

--- game6.py
+++ game6.py
@@ -4,34 +4,39 @@ def ball_animation():
     global ball_speed_x, ball_speed_y
 	  
     ball.x += ball_speed_x
-	  ball.y += ball_speed_y
+    ball.y += ball_speed_y
 
-	  if ball.top <= 0 or ball.bottom >= screen_height:
-		    ball_speed_y *= -1
-	  if ball.left <= 0 or ball.right >= screen_width:
-		    ball_speed_x *= -1
+    if ball.top <= 0 or ball.bottom >= screen_height:
+	ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+	ball_speed_x *= -1
 
-	  if ball.colliderect(player) or ball.colliderect(opponent):
-		    ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+	ball_speed_x *= -1
 
 def player_animation():
-	  player.y += player_speed
+    player.y += player_speed
 
-	  if player.top <= 0:
-		    player.top = 0
-	  if player.bottom >= screen_height:
-		    player.bottom = screen_height
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+	player.bottom = screen_height
 
 def opponent_1():
     if opponent.top < ball.y:
-		    opponent.y += opponent_speed
-	  if opponent.bottom > ball.y:
-		    opponent.y -= opponent_speed
+        opponent.y += opponent_speed
+    if opponent.bottom > ball.y:
+	opponent.y -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+	opponent.bottom = screen_height
+def ball_start():
+    global ball_speed_x, ball_speed_y
 
-	  if opponent.top <= 0:
-		    opponent.top = 0
-	  if opponent.bottom >= screen_height:
-		    opponent.bottom = screen_height
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -56,21 +61,21 @@ opponent_speed = 7
 
 while True:
     for event in pygame.event.get():
-		    if event.type == pygame.QUIT:
-			      pygame.quit()
-			      sys.exit()
+        if event.type == pygame.QUIT:
+	    pygame.quit()
+	    sys.exit()
         if event.type == pygame.KEYDOWN:
-			      if event.key == pygame.K_UP:
-				        player_speed -= 6
-			      if event.key == pygame.K_DOWN:
-				        player_speed += 6
-		    if event.type == pygame.KEYUP:
-			      if event.key == pygame.K_UP:
-				        player_speed += 6
-			      if event.key == pygame.K_DOWN:
-				        player_speed -= 6
+	    if event.key == pygame.K_UP:
+		player_speed -= 6
+	    if event.key == pygame.K_DOWN:
+	        player_speed += 6
+	if event.type == pygame.KEYUP:
+	    if event.key == pygame.K_UP:
+		player_speed += 6
+	    if event.key == pygame.K_DOWN:
+		player_speed -= 6
    
-   	ball_animation()
+    ball_animation()
     player_animation()
     opponent_1()
     
@@ -82,4 +87,4 @@ while True:
        
             
     pygame.display.flip()
-	  clock.tick(60)
+    clock.tick(60)
