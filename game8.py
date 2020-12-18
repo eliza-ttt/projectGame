0b5305901b8277f144f1171ab7a61d3f7ba17777
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2020-12-18 19:45:11 +0600

    timer

--- game8.py
+++ game8.py
@@ -1,24 +1,39 @@
 import pygame, sys, random
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y, player_score, opponent_score
+    global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
 	  
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
-	      ball_speed_y *= -1
+        ball_speed_y *= -1
+        pygame.mixer.Sound.play(plob_sound)
    
     if ball.left <= 0: 
-        ball_start()
+        score_time = pygame.time.get_ticks()
         opponent_score += 1
+        pygame.mixer.Sound.play(score_sound)
+
 	
     if ball.right >= screen_width:
-	      ball_start()
-	      ball_speed_x *= -1
+        score_time = pygame.time.get_ticks()
+	opponent_score += 1
+	pygame.mixer.Sound.play(score_sound)
 
-    if ball.colliderect(player) or ball.colliderect(opponent):
-	      ball_speed_x *= -1
+    if ball.colliderect(player) and ball_speed_x > 0:
+	if abs(ball.right - player.left) < 10:
+	    ball_speed_x *= -1
+	elif abs(ball.bottom - player.top) < 10 or abs(ball.top - player.bottom) < 10 :
+	    ball_speed_y *= -1
+	pygame.mixer.Sound.play(plob_sound)
+	
+    if ball.colliderect(opponent):
+	if abs(ball.left - opponent.right) < 10 or abs(ball.right - player.left) < 10:
+	    ball_speed_x *= -1	
+	else:
+	    ball_speed_y *= -1
+	pygame.mixer.Sound.play(plob_sound)
 
 def player_animation():
     player.y += player_speed
@@ -42,11 +57,28 @@ def opponent_1():
 	      opponent.bottom = screen_height
 	
 def ball_start():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, ball_moving, score_time
 
     ball.center = (screen_width/2, screen_height/2)
-    ball_speed_y *= random.choice((1,-1))
-    ball_speed_x *= random.choice((1,-1))
+    current_time = pygame.time.get_ticks()
+
+    if current_time - score_time < 700:
+        number_three = basic_font.render("3",False,light_grey)
+	screen.blit(number_three,(screen_width/2 - 10, screen_height/2 + 20))
+    if 700 < current_time - score_time < 1400:
+	number_two = basic_font.render("2",False,light_grey)
+	screen.blit(number_two,(screen_width/2 - 10, screen_height/2 + 20))
+    if 1400 < current_time - score_time < 2100:
+	number_one = basic_font.render("1",False,light_grey)
+	screen.blit(number_one,(screen_width/2 - 10, screen_height/2 + 20))
+
+    if current_time - score_time < 2100:
+	ball_speed_y, ball_speed_x = 0,0
+    else:
+        ball_speed_x = 7 * random.choice((1,-1))
+	ball_speed_y = 7 * random.choice((1,-1))
+	score_time = None
+
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -68,26 +100,31 @@ ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
 opponent_speed = 7
+ball_moving = False
+score_time = True
 
 player_score = 0
 opponent_score = 0
 basic_font = pygame.font.Font('freesansbold.ttf', 32)
 
+plob_sound = pygame.mixer.Sound("Plob.ogg")
+score_sound = pygame.mixer.Sound("score.ogg")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-	          pygame.quit()
-	          sys.exit()
+	    pygame.quit()
+	    sys.exit()
         if event.type == pygame.KEYDOWN:
-	          if event.key == pygame.K_UP:
-		            player_speed -= 6
-	          if event.key == pygame.K_DOWN:
-	              player_speed += 6
-	      if event.type == pygame.KEYUP:
-	          if event.key == pygame.K_UP:
-		            player_speed += 6
-	          if event.key == pygame.K_DOWN:
-		            player_speed -= 6
+	    if event.key == pygame.K_UP:
+                player_speed -= 6
+	    if event.key == pygame.K_DOWN:
+	        player_speed += 6
+	if event.type == pygame.KEYUP:
+	    if event.key == pygame.K_UP:
+	        player_speed += 6
+	    if event.key == pygame.K_DOWN:
+		player_speed -= 6
    
     ball_animation()
     player_animation()
@@ -99,6 +136,9 @@ while True:
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
        
+    if score_time:
+        ball_start()
+
     player_text = basic_font.render(f'{player_score}',False,light_grey)
     screen.blit(player_text,(660,470))
 
