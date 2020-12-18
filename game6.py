ed4a2a7cf46f88874a77362bad997f85170c5ba5
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2020-12-18 19:08:17 +0600

    Update game6.py

--- game6.py
+++ game6.py
@@ -80,10 +80,10 @@ while True:
     opponent_1()
     
     screen.fill(bg_color)
-	  pygame.draw.rect(screen, light_grey, player)
-	  pygame.draw.rect(screen, light_grey, opponent)
-	  pygame.draw.ellipse(screen, light_grey, ball)
-	  pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
        
             
     pygame.display.flip()
